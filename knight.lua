05f0ce62a9dcecfc152d82ea5ed79e0eb023b94b
Jared Norman <jared@freerunningtechnologies.com>
2014-09-12 20:41:43 -0700

    Knight knight

--- knight.lua
+++ knight.lua
@@ -1,6 +1,19 @@
 local knight = {}
 
 function knight.module()
+  local module = {}
+
+  function module.class(_, _, constructor)
+    constructor()
+    return module
+  end
+
+  function module.component(_, _, constructor)
+    constructor()
+    return module
+  end
+
+  return module
 end
 
 return knight
--- spec/knight_spec.lua
+++ spec/knight_spec.lua
@@ -5,13 +5,31 @@ describe("knight", function()
     knight.module("moduleTest")
   end)
 
-  it("allows chaining of components to modules", function()
-    knight.module("componentTest").component("Thing", [], function()
-    end)
+  it("allows defining components on modules", function()
+    local componentConstructor = spy.new(function() end)
+
+    knight.module("componentTest").component("Service", {}, componentConstructor)
+
+    assert.spy(componentConstructor).was.called()
   end)
 
-  it("allows chaining of classes to modules", function()
-    knight.module("componentTest").class("Thing", [], function()
-    end)
+  it("allows defining classes on modules", function()
+    local classConstructor = spy.new(function() end)
+
+    knight.module("componentTest").class("Thing", {}, classConstructor)
+
+    assert.spy(classConstructor).was.called()
+  end)
+
+  it("allows chaining multiple service definition", function()
+    local componentConstructor1 = spy.new(function() end)
+    local componentConstructor2 = spy.new(function() end)
+
+    knight.module("chainingTest")
+    .class("Thing1", {}, componentConstructor1)
+    .class("Thing2", {}, componentConstructor2)
+
+    assert.spy(componentConstructor1).was.called()
+    assert.spy(componentConstructor2).was.called()
   end)
 end)
