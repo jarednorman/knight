f058bf9b74a5c4767b4b283b40c7dbaaf80f1c95
Jared Norman <jared@freerunningtechnologies.com>
2014-09-13 13:22:52 -0700

    KnIgHtKnIgHtKnIgHtKnIgHtKnIgHtKnIgHtKnIgHtKnIgHt

--- knight.lua
+++ knight.lua
@@ -1,15 +1,51 @@
+-- FIXME: use normal Lua naming conventions
 local knight = {}
 
-function knight.module()
-  local module = {}
-
-  function module.class(_, _, constructor)
-    constructor()
-    return module
+local function map(array, func)
+  local new_array = {}
+  for index, value in ipairs(array) do
+    new_array[index] = func(value)
   end
+  return new_array
+end
 
-  function module.component(_, _, constructor)
-    constructor()
+local function dependenciesMet(module, dependencies)
+  for _, name in pairs(dependencies) do
+    if module.components[name] == nil then
+      return false
+    end
+  end
+  return true
+end
+
+local function getDependencies(module, dependencyList)
+  return map(dependencyList, function(dependency)
+    if module.components[dependency] then
+      return module.components[dependency].component
+    else
+      return nil
+    end
+  end)
+end
+
+local function checkDependencies(module)
+  for name, componentInfo in pairs(module.components) do
+    local dependencies = componentInfo.dependencies
+    if dependenciesMet(module, dependencies) then
+      componentInfo.component = componentInfo.constructor(unpack(getDependencies(module, dependencies)))
+    end
+  end
+end
+
+function knight.module()
+  local module = {components = {}}
+
+  function module.component(name, dependencies, constructor)
+    module.components[name] = {
+      dependencies=dependencies,
+      constructor=constructor
+    }
+    checkDependencies(module)
     return module
   end
 
--- spec/knight_spec.lua
+++ spec/knight_spec.lua
@@ -1,35 +1,21 @@
+inspect = (function()
+  local inspect = require 'inspect'
+  return function(arg) print(inspect(arg)) end
+end)()
+
 knight = require 'knight'
 
 describe("knight", function()
-  it("accepts module definitions", function()
-    knight.module("moduleTest")
-  end)
+  it("figures out dependencies like a big boy", function()
+    local thing2 = {}
+    local componentConstructor1 = spy.new(function(thing2) return 3 end)
+    local componentConstructor2 = spy.new(function() return thing2 end)
 
-  it("allows defining components on modules", function()
-    local componentConstructor = spy.new(function() end)
+    local dependencyTest = knight.module("dependencyTest")
+    .component("Thing1", {"Thing2"}, componentConstructor1)
+    .component("Thing2", {}, componentConstructor2)
 
-    knight.module("componentTest").component("Service", {}, componentConstructor)
-
-    assert.spy(componentConstructor).was.called()
-  end)
-
-  it("allows defining classes on modules", function()
-    local classConstructor = spy.new(function() end)
-
-    knight.module("componentTest").class("Thing", {}, classConstructor)
-
-    assert.spy(classConstructor).was.called()
-  end)
-
-  it("allows chaining multiple service definition", function()
-    local componentConstructor1 = spy.new(function() end)
-    local componentConstructor2 = spy.new(function() end)
-
-    knight.module("chainingTest")
-    .class("Thing1", {}, componentConstructor1)
-    .class("Thing2", {}, componentConstructor2)
-
-    assert.spy(componentConstructor1).was.called()
+    assert.spy(componentConstructor1).was.called_with(thing2)
     assert.spy(componentConstructor2).was.called()
   end)
 end)
